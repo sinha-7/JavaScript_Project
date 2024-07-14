13cd0bf558c6fb8c7bb98aa6f2591714994d4188
Supriya Singha <supriya.sinha.1609@gmail.com>
2024-07-14 07:27:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,20 +84,33 @@ function loadData() {
 }
 
 function startTime() {
-  const today = new Date();
-  let d = today.getDay();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+  hr = (hr == 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
+  //Add a zero in front of numbers<10
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+  
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var curWeekDay = days[today.getDay()];
+  var curDay = today.getDate();
+  var curMonth = months[today.getMonth()];
+  var curYear = today.getFullYear();
+  var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+  document.getElementById("date").innerHTML = date;
+  
+  var time = setTimeout(function(){ startTime() }, 500);
 }
-
 function checkTime(i) {
   if (i < 10) {
-    i = "0" + i;
-  } // add zero in front of numbers < 10
+      i = "0" + i;
+  }
   return i;
 }
